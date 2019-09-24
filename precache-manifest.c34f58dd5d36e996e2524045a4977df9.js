c9f0f37339b231e3ad408216b2eb6c5d99b3de41
Tphorton14 <Tphorton14@gmail.com>
2019-09-24 14:33:28 -0400

    Updates

---
+++ precache-manifest.c34f58dd5d36e996e2524045a4977df9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6dc9f648a46de7f83a08414ec1ca1a32",
+    "url": "/Clicky-game/index.html"
+  },
+  {
+    "revision": "f54e19ae8cb33dfa9285",
+    "url": "/Clicky-game/static/css/main.9f398850.chunk.css"
+  },
+  {
+    "revision": "4819d00ecaebd6a887e7",
+    "url": "/Clicky-game/static/js/2.0c16b9b2.chunk.js"
+  },
+  {
+    "revision": "f54e19ae8cb33dfa9285",
+    "url": "/Clicky-game/static/js/main.16b7b853.chunk.js"
+  },
+  {
+    "revision": "599eee0f941eac26ac40",
+    "url": "/Clicky-game/static/js/runtime~main.f3d11dd7.js"
+  }
+]);
