972c4715fc576042f435c36ef5e1a745963dc325
Tphorton14 <Tphorton14@gmail.com>
2019-09-23 22:02:47 -0400

    random image component fix

--- src/components/randomImages/index.js
+++ src/components/randomImages/index.js
@@ -4,7 +4,7 @@ import "./style.css";
 function RandomImage(props){
     return(
         
-        <div className="card clickedImages" onClick={() => props.clickedImages(props.id)}>
+        <div className="card clickedImages" onClick={() => props.isClicked(props.id)}>
             <div className="img-container" >
                 <img alt={props.name} src={props.image}  id={props.id} />
             </div>
